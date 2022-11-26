5cd43a56a23d388bc2cbe6ba85ca6fd69b3cfb69
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2022-11-26 10:24:54 +0100

    split out methods

--- zotero_methods.py
+++ zotero_methods.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 import shutil
-from typing import Dict
+from typing import Dict, List, Tuple
 
 from pyzotero import zotero
 
@@ -13,20 +13,22 @@ ZOTERO_PDFS_PATH = "/Users/sebastianlee/Dropbox/Zotero"
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--num_items", type=int, default=10)
+parser.add_argument("--num_items", type=int, default=5)
 parser.add_argument("--debug", action="store_true")
 
-"""
-1. Make separate file for keys as above
-2. Find out where pdf file is stored - is it just a pointer to a url 
-   or is the pdf stored locally (I think so)?
-3. Use attachment methods to link file
-"""
 
+def _copy_item_data_to_path(item: Dict) -> None:
+    filename = item["data"]["filename"]
+    path_to_file = os.path.join(LOCAL_ZOTERO_PATH, item["key"], filename)
+    shutil.copy(path_to_file, ZOTERO_PDFS_PATH)
 
-def get_recent_items(zotero_instance: zotero.Zotero, num_items: int):
-    return zotero_instance.items(limit=num_items)
-
+def _create_linked_file(zotero_instance, item: Dict, debug: bool) -> None:
+    item_template = zotero_instance.item_template(itemtype="attachment", linkmode="linked_file")
+    filename = aux_info[item["key"]]["filename"]
+    item_template["title"] = filename
+    item_template["path"] = f"attachments:{filename}"
+    if not debug:
+        zotero_instance.create_items([item_template], parentid=item["key"])
 
 def update_item(
     zotero_instance: zotero.Zotero, 
@@ -39,19 +41,15 @@ def update_item(
     if "linkMode" in item["data"]:
         if item["data"]["linkMode"] == "imported_url":
             
-            has_parent = "parentItem" in item["data"]
-            if has_parent:
-                parent_id = item["data"]["parentItem"]
-                filename = item["data"]["filename"]
-
-                path_to_file = os.path.join(LOCAL_ZOTERO_PATH, item["key"], filename)
-                shutil.copy(path_to_file, ZOTERO_PDFS_PATH)
+            parent_id = item["data"].get("parentItem")
+            if parent_id is not None:
+                _copy_item_data_to_path(item=item)
                 
                 # delete item
                 if not debug:
                     zotero_instance.delete_item(item)
 
-                aux_info_entry = {"filename": filename}
+                aux_info_entry = {"filename": item["data"]["filename"]}
                 aux_info[parent_id] = aux_info_entry
 
                 parent_keys.add(parent_id)
@@ -65,17 +63,39 @@ def update_item(
     else:
         if item["key"] in aux_info:
             if aux_info[item["key"]] is not None:
-                item_template = zotero_instance.item_template(itemtype="attachment", linkmode="linked_file")
-                filename = aux_info[item["key"]]["filename"]
-                item_template["title"] = filename
-                item_template["path"] = f"attachments:{filename}"
-                if not debug:
-                    zotero_instance.create_items([item_template], parentid=item["key"])
+                _create_linked_file(zotero_instance=zotero_instance, item=item, debug=debug)
         else:
             pass
     
     return aux_info, parent_keys
 
+
+def _group_items(items: List) -> Dict[str, Tuple]:
+    """(py)zotero defines separate item objects for parent and child items.
+    This method groups such item pairs.
+    
+    Args:
+        items: list of pyzotero items
+    
+    Returns:
+        grouped_items: dictionary where key is parent item ID, 
+        and value is list of items (including parent) that are 
+        child instances of parent item. 
+    """
+    grouped_items = {}
+    for item in items:
+        parent_id = item["data"].get("parentItem")
+        if parent_id is not None:
+            if parent_id in grouped_items:
+                grouped_items[parent_id].append(item)
+            else:
+                grouped_items[parent_id] = [item]
+        else:
+            if item["key"] in grouped_items:
+                grouped_items[item["key"]].append(item)
+            else:
+                grouped_items[item["key"]] = [item]
+    return grouped_items
 
 if __name__ == "__main__":
 
@@ -84,14 +104,15 @@ if __name__ == "__main__":
     # we now have a Zotero object, zot, and access to all its methods
     zot = zotero.Zotero(LIBRARY_ID, 'user', API_KEY)
 
-    recent_items = get_recent_items(zotero_instance=zot, num_items=2 * args.num_items)
+    # note this method call will include items in the bin
+    recent_items = zot.items(limit=2 * args.num_items)
+    grouped_items = _group_items(recent_items)
 
     aux_info = {}
     parent_keys = set()
     item_index = 0
 
-    while len(parent_keys) <= args.num_items:
-        import pdb; pdb.set_trace()
+    while len(parent_keys) < args.num_items:
         aux_info, parent_keys = update_item(
             zotero_instance=zot,
             item=recent_items[item_index],
